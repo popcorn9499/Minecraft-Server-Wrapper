649839717eb987a61c67c29f2c84f799a6028650
popcorn9499 <popcorn9499@gmail.com>
2019-06-11 15:48:59 -0500

    weird bug

--- server.py
+++ server.py
@@ -42,11 +42,14 @@ class library:
         return oldestFile
 
     def fileHumanReadable(num, suffix='B'):
-        for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
-            if abs(num) < 1024.0:
-                return "%3.1f%s%s" % (num, unit, suffix)
-            num /= 1024.0
-        return "%.1f%s%s" % (num, 'Yi', suffix)
+        try:
+            for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+                if abs(num) < 1024.0:
+                    return "%3.1f%s%s" % (num, unit, suffix)
+                num /= 1024.0
+            return "%.1f%s%s" % (num, 'Yi', suffix)
+        except:
+            return num
 
     def file_size(fname):
         return os.path.getsize(fname)
